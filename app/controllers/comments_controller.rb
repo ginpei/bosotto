af1d07db01e83bf286f5aa36bec9685d24702eb2
ginpei <ginpei@gmail.com>
2015-03-11 17:42:31 +0900

    reverse comment list's order

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,7 @@ class CommentsController < ApplicationController
   # GET /comments
   # GET /comments.json
   def index
-    @comments = Comment.all
+    @comments = Comment.all.reverse
   end
 
   # GET /comments/1

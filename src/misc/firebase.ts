1e80539473cf233bc363b814aa4e54fdb771f24e
ginpei <ginpei@gmail.com>
2021-01-10 13:41:01 -0800

    ci: 🎡 fix env vars

--- src/misc/firebase.ts
+++ src/misc/firebase.ts
@@ -35,12 +35,12 @@ export function initializeFirebase(): firebase.app.App {
   }
 
   const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
-    authDomain: process.env.REACT_APP_FIREBASE_authDomain,
-    projectId: process.env.REACT_APP_FIREBASE_projectId,
-    storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
-    appId: process.env.REACT_APP_FIREBASE_appId,
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
   };
   const app = firebase.initializeApp(firebaseConfig);
 

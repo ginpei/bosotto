c76203a3b4deb895ed6292c4eed8c1479501f730
ginpei <ginpei@gmail.com>
2021-01-10 01:07:23 -0800

    feat: 🎸 first UIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-const App: React.FC = () => {
+export const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <div className="ui-container">
+        <h1>Hello World!</h1>
+      </div>
     </div>
   );
 };
 
-export default App;
+const AppHeader: React.FC = () => {
+  return (
+    <div className="AppHeader">
+      <div className="ui-container">Bosotto</div>
+    </div>
+  );
+};

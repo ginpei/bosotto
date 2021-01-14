3b7c8db8897d4a68d1e150b82c1ccdb226191263
ginpei <ginpei@gmail.com>
2021-01-13 21:57:12 -0800

    feat: 🎸 show user name

--- src/home/AccountSection.tsx
+++ src/home/AccountSection.tsx
@@ -1,25 +1,28 @@
 import { auth } from "../misc/firebase";
-import { useCurrentUserId } from "../models/CurrentUser";
+import { useCurrentUser } from "../models/CurrentUser";
 import { DashboardSection } from "./Dashboard";
 import { LogInForm } from "./LogInForm";
 
 export const AccountSection: React.FC = () => {
-  const userId = useCurrentUserId();
+  const [user] = useCurrentUser();
 
   return (
     <DashboardSection className="AccountSection" title="Account">
-      {userId ? <LogOutForm /> : <LogInForm />}
+      {user ? <LogOutForm /> : <LogInForm />}
     </DashboardSection>
   );
 };
 
 const LogOutForm: React.FC = () => {
+  const [user] = useCurrentUser();
+
   const onLogOutClick = async () => {
     await auth.signOut();
   };
 
   return (
     <div className="LogOutForm">
+      <p>Name: {user?.displayName || "(No name)"}</p>
       <button onClick={onLogOutClick}>Log out</button>
     </div>
   );

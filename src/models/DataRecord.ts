77a3b5ae4f15596148b3d2120a62131b650b149a
ginpei <ginpei@gmail.com>
2021-01-29 07:23:45 -0800

    refactor: 💡 replace with shorter Timestamp

--- src/models/DataRecord.ts
+++ src/models/DataRecord.ts
@@ -1,4 +1,3 @@
-import firebase from "firebase";
 import { Timestamp } from "../misc/firebase";
 
 export interface DataRecord {
@@ -24,11 +23,11 @@ export function modelToDataRecord<T extends DataRecord>(
   model: T
 ): ToDocumentData<T> {
   const { id, createdAt, ...data } = model;
-  return { ...data, createdAt: new firebase.firestore.Timestamp(createdAt, 0) };
+  return { ...data, createdAt: new Timestamp(createdAt, 0) };
 }
 
 export function isDocumentData<T extends DataRecord>(
   model: Partial<T> | ToDocumentData<T> | undefined
 ): model is ToDocumentData<T> {
-  return model?.createdAt instanceof firebase.firestore.Timestamp;
+  return model?.createdAt instanceof Timestamp;
 }
